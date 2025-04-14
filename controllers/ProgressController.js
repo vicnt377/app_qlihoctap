002b520d693e38933f38a8768b696117a8b301ea
vicnt377 <caovi070703@gmail.com>
2025-04-14 16:04:56 +0700

    ..........

--- controllers/ProgressController.js
+++ controllers/ProgressController.js
@@ -1,31 +1,42 @@
 const Score = require('../models/Score');
-const Course = require('../models/Course');
 
 class ProgressController {
-  async getProgress(req, res) {
-    try {
-      // Lấy tất cả các điểm đã có
-      const scores = await Score.find()
-        .populate('HocPhan') // Lấy thông tin học phần (tenHocPhan, soTinChi)
-        .lean();
+    async getProgress(req, res) {
+        try {
+            const scores = await Score.find()
+                .populate('HocPhan')
+                .lean();
 
-      // Tính tổng số tín chỉ đã hoàn thành
-      let totalCredits = 0;
-      scores.forEach(score => {
-        if (score.HocPhan) {
-          totalCredits += score.HocPhan.soTinChi;
+            let totalCredits = 0;
+            let diemChuStats = {};
+            let monNo = [];
+
+            scores.forEach(score => {
+                if (score.HocPhan) {
+                    totalCredits += score.HocPhan.soTinChi;
+
+                    // Đếm điểm chữ
+                    const diemChu = score.diemChu.toUpperCase();
+                    diemChuStats[diemChu] = (diemChuStats[diemChu] || 0) + 1;
+
+                    // Môn nợ (F)
+                    if (diemChu === 'F') {
+                        monNo.push(score);
+                    }
+                }
+            });
+
+            res.render('progress', {
+                scores,
+                totalCredits,
+                diemChuStats,
+                monNo
+            });
+        } catch (error) {
+            console.error("Lỗi lấy tiến độ:", error);
+            res.status(500).send("Lỗi server khi lấy tiến độ học tập.");
         }
-      });
-
-      res.render('progress', {
-        scores,
-        totalCredits,
-      });
-    } catch (error) {
-      console.error("Lỗi khi lấy tiến độ:", error);
-      res.status(500).send("Lỗi server khi lấy tiến độ học tập.");
     }
-  }
 }
 
 module.exports = new ProgressController();
